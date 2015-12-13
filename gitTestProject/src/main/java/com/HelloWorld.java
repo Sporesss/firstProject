061a773c5dc490f2f31e60dca65864e45fca01b4
Sporesss <LogiSt_ST@ua.fm>
2015-12-13 12:54:59 +0200

    first commit

---
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Pasha on 13.12.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
